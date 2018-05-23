3d9527e92bffa74dc9ba477e912d26e16cae0a64
FAMILIA LASO FERNANDEZ <familialasofernandez@iMac-de-mac.local>
2018-05-24 00:23:41 +0200

    p6

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -153,3 +153,52 @@ exports.check = (req, res, next) => {
         answer
     });
 };
+
+exports.randomplay = (req,res,next) =>{
+    req.session.randomPlay = req.session.randomPlay || [];
+    const whereOpt = {'id':{[Sequelize.Op.notIn]: req.session.randomPlay}};
+    models.quiz.count({where:whereOpt})
+        .then(function (count) {
+            if(!count){
+                const score = req.session.randomPlay.length;
+                req.session.randomPlay = [];
+                res.render('quizzes/random_none',{
+                    score:score
+                });
+            };
+            return models.quiz.findAll({
+                where: whereOpt,
+                offset: Math.floor(Math.random()*count),
+                limit: 1
+            })
+                .then(function (quizzes) {
+                    return quizzes[0];
+                });
+        })
+        .then(function (quiz) {
+            res.render('quizzes/random_play',{
+                quiz: quiz,
+                score: req.session.randomPlay.length
+            });
+        })
+        .catch(function (error) {
+            next(error);
+        });
+};
+exports.randomcheck = (req, res, next) => {
+    const {quiz, query} = req;
+    const answer = query.answer || "";
+    const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
+    if(result){
+        if(req.session.randomPlay.indexOf(req.quiz.id)=== -1){
+            req.session.randomPlay = req.session.randomPlay.concat(quiz.id);
+        }
+    }
+    const score = req.session.randomPlay.length;
+    res.render('quizzes/random_result', {
+        result,
+        answer,
+        score
+    });
+};
+
